6377939b16a9d48cb8461ff0c9e2550744449156
Hary Pham <haryphamdev@gmail.com>
2021-09-23 20:48:44 +0700

    video7 static files

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
+
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
 
 const app = express();
-const port = 8080
+const port = process.env.PORT || 8080;
 
 configViewEngine(app);
 
