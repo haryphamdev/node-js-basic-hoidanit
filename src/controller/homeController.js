2adc8b351db41faf7ff7cf0e7d7496a43c02433b
Hary Pham <haryphamdev@gmail.com>
2021-10-04 17:48:47 +0700

    video15 part1 upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,6 @@
 import pool from '../configs/connectDB';
+import multer from 'multer';
+
 
 let getHomepage = async (req, res) => {
     const [rows, fields] = await pool.execute('SELECT * FROM users');
@@ -42,6 +44,41 @@ let postUpdateUser = async (req, res) => {
     return res.redirect('/');
 }
 
+let getUploadFilePage = async (req, res) => {
+    return res.render('uploadFile.ejs')
+}
+
+
+const upload = multer().single('profile_pic');
+
+let handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
+
+
 module.exports = {
-    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser,
+    getUploadFilePage, handleUploadFile
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,33 @@
 import express from "express";
 import homeController from '../controller/homeController';
+import multer from 'multer';
+import path from 'path';
+var appRoot = require('app-root-path');
 let router = express.Router();
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
@@ -9,7 +36,10 @@ const initWebRoute = (app) => {
 
     router.post('/delete-user', homeController.deleteUser);
     router.get('/edit-user/:id', homeController.getEditPage);
-    router.post('/update-user', homeController.postUpdateUser)
+    router.post('/update-user', homeController.postUpdateUser);
+
+    router.get('/upload', homeController.getUploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
 
 
     router.get('/about', (req, res) => {
