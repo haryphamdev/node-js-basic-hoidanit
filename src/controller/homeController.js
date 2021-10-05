db822ac74905b2f5dd3e8c3a6c5652410dcbd2b6
Hary Pham <haryphamdev@gmail.com>
2021-10-05 20:40:23 +0700

    video15 part2 upload multiple files

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -49,36 +49,45 @@ let getUploadFilePage = async (req, res) => {
 }
 
 
-const upload = multer().single('profile_pic');
-
 let handleUploadFile = async (req, res) => {
-    // 'profile_pic' is the name of our file input field in the HTML form
 
-    upload(req, res, function (err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
+    if (req.fileValidationError) {
 
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
 
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
-    });
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    // });
 }
 
 
+let handleUploadMultipleFiles = async (req, res) => {
+
+    if (req.fileValidationError) {
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.files) {
+        return res.send('Please select an image to upload');
+    }
+
+    let result = "You have uploaded these images: <hr />";
+    const files = req.files;
+    let index, len;
+
+    // Loop through all the uploaded images and display them on frontend
+    for (index = 0, len = files.length; index < len; ++index) {
+        result += `<img src="/image/${files[index].filename}" width="300" style="margin-right: 20px;">`;
+    }
+    result += '<hr/><a href="/upload">Upload more images</a>';
+    res.send(result);
+
+}
 
 module.exports = {
     getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser,
-    getUploadFilePage, handleUploadFile
+    getUploadFilePage, handleUploadFile, handleUploadMultipleFiles
 }
--- src/route/web.js
+++ src/route/web.js
@@ -28,6 +28,7 @@ const imageFilter = function (req, file, cb) {
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
 
+let uploadMultipleFiles = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images', 3);
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
@@ -40,6 +41,23 @@ const initWebRoute = (app) => {
 
     router.get('/upload', homeController.getUploadFilePage);
     router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+
+    router.post('/upload-multiple-images', (req, res, next) => {
+        uploadMultipleFiles(req, res, (err) => {
+            if (err instanceof multer.MulterError && err.code === "LIMIT_UNEXPECTED_FILE") {
+                // handle multer file limit error here
+                res.send('LIMIT_UNEXPECTED_FILE')
+            } else if (err) {
+                res.send(err)
+            }
+
+            else {
+                // make sure to call next() if all was well
+                next();
+            }
+        })
+    }, homeController.handleUploadMultipleFiles)
+
 
 
     router.get('/about', (req, res) => {
