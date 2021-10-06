765641d3e808fe54d26088ff33f61c82e2831e83
Hary Pham <haryphamdev@gmail.com>
2021-10-06 11:07:20 +0700

    video 16 middleware

--- src/server.js
+++ src/server.js
@@ -5,9 +5,21 @@ import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 
 require('dotenv').config();
+var morgan = require('morgan')
+
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+    //check => return res.send()
+    console.log('>>> run into my middleware')
+    console.log(req.method)
+    next();
+})
+
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +32,11 @@ initWebRoute(app);
 
 // init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 
 app.listen(port, () => {
